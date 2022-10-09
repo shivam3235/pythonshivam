2addae82e43a7f65e673b534bcc46f050fbecb6c
shivam3235 <114767505+shivam3235@users.noreply.github.com>
2022-10-09 10:22:13 +0530

    Add files via upload

---
+++ variables python.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 100
+type(x)
+
+
+# In[2]:
+
+
+x = 654*6734
+type(x)
+
+
+# In[3]:
+
+
+print(x)
+
+
+# In[4]:
+
+
+x =3.14
+print(x)
+type(x)
+
+
+# In[5]:
+
+
+x = "Shivam"
+print(x)
+
+
+# In[7]:
+
+
+x ='shivam'
+print(x)
+
+
+# In[8]:
+
+
+type(x)
+
+
+# In[9]:
+
+
+x = [12,23,23]
+print(x)
+
+
+# In[10]:
+
+
+type(x)
+
+
+# In[11]:
+
+
+print(x[2])
+
+
+# In[13]:
+
+
+print(x[0
+        
+
+
+# In[17]:
+
+
+x[2]=23
+
+
+# In[18]:
+
+
+print(x)
+
+
+# In[19]:
+
+
+x = (4,8,6)
+print(x)
+
+
+# In[20]:
+
+
+type(x)
+
+
+# In[21]:
+
+
+print(x[1])
+
+
+# In[22]:
+
+
+x[1]=5
+
+
+# In[24]:
+
+
+x = open('variable_com.ipynb','r')
+type(x)
+
+
+# In[25]:
+
+
+(x,y,z) =5,10,7
+print(x)
+print(y)
+print(z)
+
+
+# In[26]:
+
+
+(x,y,z)=1,1,1
+
+
+# In[27]:
+
+
+x=y=z=1
+
+
+# In[28]:
+
+
+print(x,y,z)
+
+
+# # rule for naming variable
+
+# In[29]:
+
+
+a100 = 100
+A100 = 100
+print(a100,A100)
+
+
+# In[30]:
+
+
+break=10
+
+
+# In[31]:
+
+
+class=5
+
+
+# In[32]:
+
+
+try=100
+
+
+# # Arithmetic operation
+# 
+
+# In[35]:
+
+
+x= 10
+y = 20
+result=x+y
+print(result)
+
+
+# In[39]:
+
+
+result = x-y
+print(result)
+
+
+# In[42]:
+
+
+result = x*y
+print(result)
+
+
+# In[43]:
+
+
+result = x%y
+print(result)
+
+
+# In[44]:
+
+
+result = x/y
+print(result)
+
+
+# In[46]:
+
+
+result = x//y
+print(result)
+
+
+# In[47]:
+
+
+result = 2/3
+print(result)
+
+
+# In[48]:
+
+
+result = 2//3
+print(result)
+
+
+# In[49]:
+
+
+result = x%y
+print(result)
+
+
+# In[51]:
+
+
+result =3%2
+print(result)
+
+
+# In[52]:
+
+
+x = 3.14
+y= 5.7
+
+
+# In[53]:
+
+
+result =x/y
+print(result)
+
+
+# # String operation
+
+# In[54]:
+
+
+var = "shivam prajapati"
+print(var[0])
+
+
+# In[55]:
+
+
+print(var[4])
+
+
+# In[56]:
+
+
+print(var[0:3])
+
+
+# In[57]:
+
+
+print(var[:3])
+
+
+# In[58]:
+
+
+print(var[5:])
+
+
+# In[59]:
+
+
+print(var[0:20])
+
+
+# In[60]:
+
+
+len(var)
+
+
+# In[ ]:
+
+
+
+

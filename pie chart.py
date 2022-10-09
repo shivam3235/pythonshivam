3467e15fd3c135cf47836a47a0ad4c5686cadb95
Shivamprajapati147 <114767505+shivam3235@users.noreply.github.com>
2022-10-09 11:01:46 +0530

    Add files via upload

---
+++ pie chart.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# In[3]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+
+
+# In[4]:
+
+
+Classe1_student =[45,15,35,25,30]
+
+
+# In[5]:
+
+
+plt.pie([1])
+plt.show()
+
+
+# In[6]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+plt.pie(Classe1_student)
+plt.show()
+
+
+# In[7]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+plt.pie(Classe1_student,labels =classes)
+plt.show()
+
+
+# In[14]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,autopct="%0.1f%%")
+plt.show()
+
+
+# In[15]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,autopct="%0.1f%%",shadow=True)
+plt.show
+
+
+# In[17]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4)
+plt.show()
+
+
+# In[18]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4,startangle=90)
+plt.show()
+
+
+# In[19]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4,startangle=270)
+plt.show()
+
+
+# In[24]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+textprops = {"fontsize":15}
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4,startangle=270,textprops=textprops)
+plt.show()
+
+
+# In[25]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+textprops = {"fontsize":15}
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4,startangle=270,textprops=textprops)
+plt.show()
+
+
+# In[ ]:
+
+
+classes =['Python','R','Machine Learning','Artificial Intelligence','Data Scince']
+Classe1_student =[45,15,35,25,30]
+explode =[0,0.03,0.1,0,0]
+colors =['c','b','r','y','g']
+textprops = {"fontsize":15}
+plt.pie(Classe1_student,labels =classes,explode=explode,colors=colors,
+        autopct="%0.1f%%",shadow=True,radius=1.4,startangle=270,textprops=textprops)
+plt.show()
+
+
+# In[29]:
+
+
+wefgeprops ={"linewidth":4,'width':1,"edgecolor":"k"
+plt.pie(
+
+       )
+
+
+# In[ ]:
+
+
+
+

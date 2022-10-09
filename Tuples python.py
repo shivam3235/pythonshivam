323f2a0929177247fa825bf6595d2c8465419af0
shivam3235 <114767505+shivam3235@users.noreply.github.com>
2022-10-09 10:26:30 +0530

    Add files via upload

---
+++ Tuples python.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+
+# In[2]:
+
+
+emp=()
+print(type(emp))
+
+
+# In[3]:
+
+
+print(emp)
+
+
+# In[4]:
+
+
+city='pune'
+type(city)
+
+
+# In[6]:
+
+
+city=("pune")
+type(city)
+
+
+# In[8]:
+
+
+city="lucknow"
+city
+
+
+# In[9]:
+
+
+city= "pune","lucknow","munbai"
+city
+
+
+# In[10]:
+
+
+list1=[1,2,3,4,]
+tuple1=[1,2,3,4]
+list1.append(5)
+
+
+# In[11]:
+
+
+print(list1)
+
+
+# In[12]:
+
+
+print(city)
+
+
+# In[13]:
+
+
+city[1]
+
+
+# In[14]:
+
+
+city[-1]
+
+
+# # concatenation
+
+# In[15]:
+
+
+print(city)
+
+
+# In[16]:
+
+
+num=1,2,3
+
+
+# In[17]:
+
+
+print(city+num)
+
+
+# #Nesting
+
+# In[18]:
+
+
+nest=(city,num)
+
+
+# In[19]:
+
+
+print(nest)
+
+
+# In[23]:
+
+
+rep=("python",)=5
+rep
+
+
+# In[24]:
+
+
+rep=("shivam",)*5
+rep
+
+
+# In[25]:
+
+
+rep=("python",)
+
+
+# In[27]:
+
+
+print(rep*10)
+
+
+# In[28]:
+
+
+rep
+
+
+# In[29]:
+
+
+num
+
+
+# In[30]:
+
+
+NUM[1:]
+
+
+# In[31]:
+
+
+num[1:]
+
+
+# In[32]:
+
+
+num[::-1]
+
+
+# In[33]:
+
+
+tuple("simplilearn")
+
+
+# In[34]:
+
+
+print(num)
+
+
+# In[37]:
+
+
+a,b,c,d=num
+print(a,b,c,d)
+
+
+# In[36]:
+
+
+
+
+
+# In[38]:
+
+
+tuple=(1,2,3,4)
+print(tuple)
+
+
+# In[39]:
+
+
+del tuple
+print(tuple)
+
+
+# In[40]:
+
+
+num1=(1,2,3,4,5,6,7,8)
+num1.count(2)
+
+
+# In[41]:
+
+
+sum(num)
+
+
+# In[42]:
+
+
+len(num)
+
+
+# In[43]:
+
+
+max(num)
+
+
+# In[44]:
+
+
+min(num)
+
+
+# In[45]:
+
+
+list=[1,2,32,34]
+print(type(list))
+
+
+# In[46]:
+
+
+tpl=tuple(list)
+print(tpl)
+
+
+# In[47]:
+
+
+type(tpl)
+
+
+# In[48]:
+
+
+list=[(1,2,3),(4,5,6)]
+
+
+# In[49]:
+
+
+print(list)
+
+
+# In[51]:
+
+
+list.append(("tuple","inside","list"))
+print(list)
+
+
+# 
+
+# In[52]:
+
+
+list((1,2,3))
+
+
+# In[53]:
+
+
+print(list)
+
+
+# In[54]:
+
+
+list.remove((1,2,3))
+print(list)
+
+
+# In[55]:
+
+
+tpl=(['a','b','c'],['d','e','f'])
+print(tpl)
+
+
+# In[56]:
+
+
+tpl[0].append
+
+
+# In[58]:
+
+
+tpl[0].append['z']
+print(tpl)
+
+
+# In[59]:
+
+
+tpl[0].append('z')
+print(tpl)
+
+
+# In[60]:
+
+
+tpl[0].remove('a')
+print(tpl)
+
+
+# In[ ]:
+
+
+
+
